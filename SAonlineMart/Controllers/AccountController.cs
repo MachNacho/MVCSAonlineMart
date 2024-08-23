8c68290a36ca57ad47520469efc24836c4a31b34
Asi <64479526+MachNacho@users.noreply.github.com>
2024-08-23 12:25:45 +0200

    small fix to account

--- SAonlineMart/Controllers/AccountController.cs
+++ SAonlineMart/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using SAonlineMart.Data;
 using SAonlineMart.Models;
 using SAonlineMart.ViewModels;
@@ -17,25 +18,23 @@ namespace SAonlineMart.Controllers
 			_userManager = userManager;
 			_signInManager = signInManager;
 		}
-		public IActionResult Login()
+		public IActionResult Login()//used for showing page
 		{
 			var response = new LoginViewModel();
 			return View(response);
 		}
 
 		[HttpPost]
-		public async Task<IActionResult> Login(LoginViewModel model)
+        public async Task<IActionResult> Login(LoginViewModel model)//Used for logging in
 		{
 			if(!ModelState.IsValid) return View(model);
-
-			var user = await _userManager.FindByEmailAsync(model.Email);
-
-			if (user != null) 
+			var user = await _userManager.FindByEmailAsync(model.EmailAddress);
+			if (user != null)
 			{
 				var PasswordCheck = await _userManager.CheckPasswordAsync(user, model.Password);
 				if (PasswordCheck)
 				{
-					var result = await _signInManager.PasswordSignInAsync(user,model.Password,false,false);
+					var result = await _signInManager.PasswordSignInAsync(user, model.Password, false, false);
 					if (result.Succeeded)
 					{
 						return RedirectToAction("Index","Product");
@@ -47,5 +46,33 @@ namespace SAonlineMart.Controllers
 			TempData["Error"] = "Wrong Credatnials";
 			return View(model);
 		}
-	}
+
+		public IActionResult Register() { var response = new RegisterViewModel(); return View(response); }
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterViewModel model) 
+		{
+			if (!ModelState.IsValid) return View(model);
+			var user = await _userManager.FindByEmailAsync(model.EmailAddress);
+			if (user != null) {
+				TempData["Error"] = "This email address already in use";
+				return View(model);
+			}
+			var newcustomer = new Customer()
+			{
+				Email = model.EmailAddress,
+				UserName = model.EmailAddress
+			};
+			var newUserResponse = await _userManager.CreateAsync(newcustomer,model.Password);//create user
+			if (newUserResponse.Succeeded) { await _userManager.AddToRoleAsync(newcustomer, UserRoles.User); }//add a role
+			return RedirectToAction("Index", "Home");
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Logout() 
+		{
+			await _signInManager.SignOutAsync();
+			return RedirectToAction("Index","Home");
+		}
+    }
+
 }
